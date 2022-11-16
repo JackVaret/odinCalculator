a026ec518ba88155eb6913c275bc2cc404ad4afe
jackvaret <victorogunsanya13@gmail.com>
2022-11-15 22:29:50 -0500

    Created function to update the display

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Create functions for mathematical operations
 function add (firstNumber,secondNumber)
 {
     return firstNumber + secondNumber;
@@ -14,7 +15,7 @@ function divide (firstNumber,secondNumber)
 {
     return firstNumber / secondNumber;
 }
-
+// Create function to perform a given mathematical operation with two numbers
 function operate (operation,firstNumber,secondNumber)
 {
     if(operation === "add")
@@ -34,3 +35,32 @@ function operate (operation,firstNumber,secondNumber)
         return divide(firstNumber,secondNumber);
     }
 }
+// Create a function to populate display when number buttons are clicked
+// Store said display value into a variable
+let displayValue = "";
+// Target the display
+const display = document.querySelector(".display");
+// Create a selector to select all of the number buttons
+const allNums = document.querySelectorAll(".num");
+// Create a function to add another string to hte displayValue
+
+function populateDisplay(e) 
+{
+    let value = e.target.innerHTML;
+    displayValue = displayValue + value;
+    display.innerHTML = displayValue;
+    
+}
+
+// When a number button is clicked, send that number's text content to the displayValue
+allNums[0].addEventListener("click", populateDisplay);
+
+// Add this event to all of the numBUttons
+for(const num of allNums)
+{
+    num.addEventListener("click", populateDisplay);
+}
+// Store the displayValue as a number
+let currentNum = displayValue;
+// Create a temporary number that will gain the value of currentNum when an operation is called 
+let tempNum = 0;
