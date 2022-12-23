bc8139468a991260146dbf28187c3453097c4a29
jackvaret <victorogunsanya13@gmail.com>
2022-12-22 23:29:53 -0500

    Added logic for the operation buttons

--- index.js
+++ index.js
@@ -44,12 +44,58 @@ let display = document.querySelector(".display");
 let displayValue = parseFloat(display.textContent);
 function populateDisplay(e)
 {
+    
     display.textContent +=e.target.textContent;
     displayValue = parseFloat(display.textContent);
+    if(operationPressed)
+    {
+        // If an operation is pressed, clear the display and set the display to the number
+        clearDisplay()
+        display.textContent+=e.target.textContent;
+        displayvalue = parseFloat(display.textContent);
+    }
+      
 }
 // Add this function to all of the number buttons
 document.querySelectorAll(".num").forEach(num => 
     {
         num.addEventListener('click', populateDisplay)
     });
-    
+// Create a function to clear the display
+function clearDisplay()
+{
+    display.textContent = "";
+}
+// Store a first and second number as variables
+let firstNum = 0;
+let secondNum = 0;
+// First number is the number before an operation is inputted, and the second number is the number after an operation is inputted
+// Create a variable to store the current operation and a variable to detect if an operation has been pressed
+let currentOperation = "";
+let operationPressed = false;
+
+// Logic for the operation buttons:
+// When you press an operation button, store the current displayValue as the first number
+if(operationPressed = false) 
+    {
+    // Set operationPressed to true
+    operationPressed = true;
+    firstNum = displayValue;
+    // If no operation is pressed, don't allow the user to press the equal button 
+    }
+    else if(operationpressed)
+    {
+        // If an operation is already pressed, evaluate the first and second numbers
+        secondNum = displayValue;
+        // Set the display to the previous operation evaluation
+        display.textContent=operate(currentOperation,firstNum,secondNum);
+        displayValue = parseFloat(display.textContent);
+        // Set the current operation to the operation just pressed and set the first number to be that result
+        firstNum = displayValue;
+        // Set the current operation to be the nextoperation
+
+        operationPressed = true;
+
+
+    }
+   // Add that logic as eventlisteners for the operation buttons
